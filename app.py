912032b40406f28e0fd797d8fbf6f440d18fa3a2
ecarmu <92380011+ecarmu@users.noreply.github.com>
2024-01-14 08:48:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+print(dir(Flask))
 from flask import Flask, jsonify, request
 from datetime import datetime
 from flask_cors import CORS  # Import the CORS module
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
-print(dir(Flask))
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 print(app)
